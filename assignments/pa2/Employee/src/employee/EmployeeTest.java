07d3161c7e4b3bf8b5a3d86bb314a391f701b394
Mindy Tomlinson <mindy.tomlinson@gmail.com>
2017-09-03 22:26:56 -0400

    Verify tests match sample output

--- assignments/pa2/Employee/src/employee/EmployeeTest.java
+++ assignments/pa2/Employee/src/employee/EmployeeTest.java
@@ -11,19 +11,24 @@ public class EmployeeTest {
         System.out.println(mindy);
         
         CommissionEmployee amber = new CommissionEmployee("Amber","Miller",
-                "888-88-8888", 100000.0, 0.2);
+                "888-88-8888", 2000.0, 0.05);
         System.out.println(amber);
         
         BasePlusCommissionEmployee shana = new BasePlusCommissionEmployee(
-                "Shana","Nissenbaum","777-77-7777",50000,0.3,80000);
+                "Shana","Nissenbaum","777-77-7777", 3000, 0.05, 300);
         System.out.println(shana);
         
         SalariedEmployee julie = new SalariedEmployee("Julie", "Trubitt", 
-                "666-66-6666", 90000);
+                "666-66-6666", 1150);
         System.out.println(julie);
         
         HourlyEmployee maggie = new HourlyEmployee("Maggie","Thomas",
-                "555-55-5555", 10, 50);
+                "555-55-5555", 15, 50);
         System.out.println(maggie);
+        
+        HourlyEmployee marissa = new HourlyEmployee("Marissa", "Rodriguez",
+                "444-44-4444", 20, 40);
+        System.out.println(marissa);
     }
+    
 }
